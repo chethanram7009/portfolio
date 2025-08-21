4e6d51ef234f92bfe6c44190fd4bd6a94e48d355
chethan <chethanv@iisc.ac.in>
2025-08-21 12:41:49 +0530

    Updated site

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
         <li><strong>Contact No:</strong> 7090121244</li>
         <li><strong>Email:</strong> chethanv@iisc.ac.in / chethanram7009@gmail.com</li>
         <li><strong>LinkedIn:</strong> https://www.linkedin.com/in/chethan-v-a-9996b8215/</li>
-        <li><strong>GitHub:</strong> https://www.linkedin.com/in/chethan-v-a-9996b8215/</li>
+        <li><strong>GitHub:</strong> https://github.com/chethanram7009/</li>
         <li><strong>ResearchGate:</strong> https://www.researchgate.net/profile/Chethan-V-A-2?ev=hdr_xprf/</li>
       </ul>
     </section>
